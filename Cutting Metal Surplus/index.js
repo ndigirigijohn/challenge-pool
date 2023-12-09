7b71567701782c447ccdfb8534370acb20de8bb8
ndigirigijohn <jontejonhn@gmail.com>
2023-12-09 17:05:57 +0300

    ascii text and metal surplus solutuion updates

--- Cutting Metal Surplus/index.js
+++ Cutting Metal Surplus/index.js
@@ -1,39 +1,33 @@
-function maxProfit(S) {
-    const { costPerCut, salePrice, lengths } = S;
+function maxProfit(costPerCut, salePrice, lengths) {
     let maxProfit = 0;
-
-    for (let saleLength = 1; saleLength <= 10000; saleLength++) {
-        let totalUniformRods = 0;
-        let totalCuts = 0;
-
-        lengths.forEach(length => {
-            const cuts = Math.floor(length / saleLength);
-            const leftover = length % saleLength;
-
-            totalUniformRods += cuts;
-
-            if (leftover > 0) {
-                totalCuts += cuts; // Adjusted to use 'cuts' instead of 'leftover'
-            }
-        });
-
-        const profit = totalUniformRods * saleLength * salePrice - totalCuts * costPerCut;
-
-        if (profit > maxProfit) {
-            maxProfit = profit;
+  
+    for (let saleLength = 1; saleLength <= Math.max(...lengths); saleLength++) {
+      const salePricePerRod = salePrice * saleLength;
+      let profit = 0;
+  
+      for (const rodLength of lengths) {
+        const uniformRods = Math.floor(rodLength / saleLength);
+  
+        if (uniformRods > 0) {
+          const extraCut = rodLength % saleLength > 0 ? 1 : 0;
+          const totalCuts = uniformRods - 1 + extraCut;
+  
+          const costs = totalCuts * costPerCut;
+          const revenues = uniformRods * salePricePerRod;
+  
+          if (revenues > costs) {
+            profit += revenues - costs;
+          }
         }
+      }
+  
+      if (profit > maxProfit) {
+        maxProfit = profit;
+      }
     }
-
+  
     return maxProfit;
-}
+  }
+  
 
-
-
-
-const S = {
-    costPerCut: 1,
-    salePrice: 10,
-    lengths: [20, 59, 110]
-};
-
-console.log(maxProfit(S));
+  console.log(maxProfit(1, 10, [26, 103, 59]));
